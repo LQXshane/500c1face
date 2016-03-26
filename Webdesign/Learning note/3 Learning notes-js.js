56a8ed875c6a4e40084bdfced0c8dbc7c13fddba
GordonCai <s20525xx@bu.edu>
2016-03-26 09:07:29 -0400

    update

--- Webdesign/Learning note/3 Learning notes-js.js
+++ Webdesign/Learning note/3 Learning notes-js.js
@@ -1,30 +1,30 @@
 Javascript notes, *open with IDE, not browser*
 
-Intro:  use  getElementById() to "find" an HTML element (with id="demo"), -
-        and changes the element content (innerHTML) to "Hello JavaScript":
+--Intro:  use  getElementById() to "find" an HTML element (with id="demo"), -
+         and changes the element content (innerHTML) to "Hello JavaScript":
          document.getElementById("demo").innerHTML = "Hello JavaScript";
 
         Changing the style of an HTML element:
-         <p id="demo"> sth </p>
+         <p id="demo"> sth </p> /
          <button type="button" onclick="
          document.getElementById("demo").style.fontSize = "25px";
          document.getElementById("demo").innerHTML = "Hello World";"
 
 
-Where to: tag; <script>  </script> ;
+Where to: tag; <script>  </script> ;/
 
           function can be executed when an event occurs;
            <script>
              function myFunction() {
              document.getElementById("demo").innerHTML = "Paragraph changed.";
              }
-           </script> \\ define function 
+           </script>/ \\ define function 
            <button type="button" onclick="myFunction()">Try it</button> call/triger event
-           <p id="demo">A Paragraph.</p> \\ event's target
+           <p id="demo">A Paragraph.</p> /* event's target */
 
-          It is a good idea to place scripts at the bottom of the <body> element. 
+--          It is a good idea to place scripts at the bottom of the <body> element. 
 
-          External scripts are practical when the same code is used in many different web pages.
+--          External scripts are practical when the same code is used in many different web pages.
 
 
 Output: alert box: window.alert();
@@ -35,7 +35,7 @@ Output: alert box: window.alert();
 
         for testing purposes:  document.write();
                        
-        access an HTML element: document.getElementById(id);
+--        access an HTML element: document.getElementById(id);
 
         use the console.log() method to display data;
 
@@ -48,18 +48,18 @@ Syntax: JavaScript statements are composed of: -
 
         operators: = is to assign values to var;
                    +-*/ to compute values;
-
+        /
         expression: literal+var+operators;
 
         keywords: used to identify actions to be performed: var y = x * 10
 
         comment: // and  /*  */
 
-        identifier: first character must be a letter, an underscore (_), or a dollar sign ($), not number;
+--        identifier: first character must be a letter, an underscore (_), or a dollar sign ($), not number;
 
         case sensitive;
 
-        camel case: ie innerCity, lastName;
+--        camel case: ie innerCity, lastName;
 
         
 Statements: statements are "instructions" to be "executed" by the web browser;
@@ -113,7 +113,8 @@ Operaters: arithmetic: + - * ? %(division remainder) ++ --;
              ?	ternary operator
 
 
-Data type: typical types of variables: number, string, array and object;
+Data type: 
+--           typical types of variables: number, string, boolean,array and object;
 
            when adding a number and a string, JavaScript will treat the number as a string; 
            can use single or double quotes;
@@ -135,19 +136,21 @@ Data type: typical types of variables: number, string, array and object;
 
 Function: a block of code designed to perform a particular task;
 
-          general format: function name(parameter1, parameter2, parameter3) { code to be executed }
+--          general format: function name(parameter1, parameter2, parameter3) { code to be executed }
           
-          return statement: return value to caller id="demo"
-           <p id="demo"></p>
+--          return statement: return value to caller id="demo"
+           <p id="demo"></p>/
            <script>
              function myFunction(a, b) {
              return a * b;
              }
            document.getElementById("demo").innerHTML = myFunction(4, 3);
-           </script>
+           </script>/
 
            function can also be used as variable:
              var text = "The temperature is " + toCelsius(77) + " Celsius";
+
+           more about functions later in this learning note;  
 
 
 Object: object is variable too ,but has many values
@@ -156,10 +159,12 @@ Object: object is variable too ,but has many values
 
         avoid String, Number, and Boolean objects;
 
+--        more object topic covered later in this learning note;
+
 
 Scope: set of variables, objects, and functions you have access to;
 
-       local and global variables: 
+--       local and global variables: 
         local declarition is done inside a function;
         global variables is either declared outside or no declaration;
 
@@ -169,21 +174,22 @@ Scope: set of variables, objects, and functions you have access to;
          window.carName = "Volvo";
 
 
-Event: "things" that happen to HTML elements, JavaScript can "react" on these events;
+--Event: "things" that happen to HTML elements, JavaScript can "react" on these events;
 
        HTML event can be something the browser does, or something a user does, JavaScript lets you execute code when events are detected.
        
        event handler attributes: <some-HTML-element some-event="some JavaScript">;
          <button onclick='getElementById("demo").innerHTML=Date()'>The time is?< button>
 
-       common html event: 
+--       common html event: 
          onchange   An HTML element has been changed
          onclick   The user clicks an HTML element
-         onmouseover   The user moves the mouse over an HTML element
+--         onmouseover   The user moves the mouse over an HTML element
          onmouseout  The user moves the mouse away from an HTML element
          onkeydown   The user pushes a keyboard key
          onload  The browser has finished loading the page
-       more in html learning note DOM
+
+--       more in html learning note DOM
 
 
 Property and methods: property relates to the values of data; 
@@ -386,6 +392,187 @@ More tips on mistakes and performance at W3SCHOOL.COM
 
 
 Reserved words: see W3SCHOOL.COM
+
+//////////////////////////////////////////////////////////////////////////////
+
+Form validation:
+<head>
+<script>
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value; /* more document see DOM learning note*/
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+</script>
+</head>
+<body>
+<form name="myForm" action="demo_form.asp"
+onsubmit="return validateForm()" method="post"> /* onsubmit is type of event*/
+/* in this case, onsubmit return submited text to validateForm function */
+Name: <input type="text" name="fname">
+<input type="submit" value="Submit">
+</form>
+</body>
+
+
+
+Form API:
+<input id="id1" type="number" min="100" max="300"> /* send input to getElementById*/
+<button onclick="myFunction()">OK</button>
+<p id="demo"></p>
+<script>
+function myFunction() {
+    var inpObj = document.getElementById("id1");
+    if (inpObj.checkValidity() == false) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    } else {
+        document.getElementById("demo").innerHTML = "Input OK"; /*return message to demo*/
+    } 
+} 
+</script>
+
+///////////////////////////////////////////////////////////////////////////
+
+Create object:
+
+using literals:
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}; 
+
+using keyword 'new':
+var person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";                
+
+using object constructor: /* function is type of constructor */
+function person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+var myFather = new person("John", "Doe", 50, "blue");
+var myMother = new person("Sally", "Rally", 48, "green");
+
+build in js constructor:
+var x1 = {};    // A new Object object
+var x2 = new String();    // A new String object
+var x3 = new Number();    // A new Number object
+var x4 = new Boolean()    // A new Boolean object
+var x5 = [];     // A new Array object
+var x6 = /()/;    // A new RegExp object
+var x7 = function(){};  // A new Function object
+var x8 = new Date();      // A new Date object 
+
+/////////////////////////////////////////////////////////////////////////////////
+Function:
+function functionName(parameters) {
+  code to be executed
+} /* {} is a preperty of object, so function is object aswell */
+
+function constructor: see above;
+
+
+Function parameters: parameters are the names listed in the function definition:
+
+Function arguments:  arguments are the real values passed to (and received by) the function;
+x = findMax(1, 123, 500, 115, 44, 88); /* this invoke functuion*/
+
+function findMax() {
+    var i;
+    var max = -Infinity;
+    for (i = 0; i < arguments.length; i++) { 
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+} 
+
+
+Invoke funsction
+
+/////////////////////////////////////////////////////////////////////////////////
+
+DOM: Document Object Model; 
+     syntax: document.METHODPROPERTY
+
+
+Intro: The HTML DOM is a standard for how to get, change, add, or delete HTML elements
+
+
+Methods: methods are actions you can perform; 
+         properties are values (of HTML Elements) that you can set or change;
+         all HTML elements are defined as objects;
+
+         most common method: getElementById
+         easiest way to change content of element: innerHTML
+         
+
+Document: 
+find elements:
+--document.getElementById(id)   Find an element by element id, return in form of content
+document.getElementsByTagName(name)   Find elements by tag name, return is in form of array
+document.getElementsByClassName(name)   Find elements by class name, return is in form of array
+
+change element: 
+--element.innerHTML =  new html content   Change the inner HTML of an element
+element.attribute = new value   Change the attribute value of an HTML element
+element.setAttribute(attribute, value)  Change the attribute value of an HTML element
+element.style.property = new style  Change the style of an HTML element
+
+add/deelet element:
+document.createElement(element)   Create an HTML element
+document.removeChild(element)   Remove an HTML element
+document.appendChild(element)   Add an HTML element
+document.replaceChild(element)  Replace an HTML element
+document.write(text)  Write into the HTML output stream
+
+event handler: 
+document.getElementById(id).onclick = function(){code}  Adding event handler code to an onclick event
+
+
+--Changing HTML content:
+<p id="p1">Hello World!</p>
+<script>
+document.getElementById("p1").innerHTML = "New text!";
+</script>/
+
+
+--Change value of attribute:
+<img id="myImage" src="smiley.gif">
+<script>
+document.getElementById("myImage").src = "landscape.jpg";
+</script>/
+
+
+--Use event:
+<h1 id="id1">My Heading 1</h1>
+<button type="button"
+onclick="document.getElementById('id1').style.color = 'red'">
+Click Me!</button>
+
+
+Event listener;
+syntax:  element.addEventListener(event, function, useCapture);
+
+
+Nodes:
+everything inside html is a node;
+node tree, child and parent;
+
+
+BOM: Browser Object Model
+
+
+
+
+
+
+
 
 
 
