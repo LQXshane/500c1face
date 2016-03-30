ac9ab449ec8f955bdc69ca2a0de5640d0a9ddc12
Qiuxuan Lin <qxlin@bu.edu>
2016-03-29 20:38:20 -0400

    Add these files for testing EC2

---
+++ Webdesign/Pre3-Website/Pres3 website/portfolio.php
@@ -0,0 +1,35 @@
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->    
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">    
+                    <div class="item active">
+                        <img class="img-portfolio img-responsive" src="img/slideshow1.png" alt="void">
+                    </div>
+                        
+                    <div class="item">
+                      <img class="img-portfolio img-responsive" src="img/slideshow2.png" alt="void">
+                    </div>
+
+                    <div class="item">
+                      <img class="img-portfolio img-responsive" src="img/slideshow3.png" alt="void">
+                    </div>
+                        
+                    <div class="item">
+                      <img class="img-portfolio img-responsive" src="img/slideshow4.png" alt="void">
+                    </div>
+                </div>
+                <!-- carousel-inner-->
+            </div>
+            <!-- myCarousel -->
+        </div>
+        <!-- /.container -->
+    </section>
