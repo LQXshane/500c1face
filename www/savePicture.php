6e67e04f54b02a006423c763f87653ad9e3d4118
GordonCai <s20525xx@bu.edu>
2016-04-07 21:49:03 -0400

    update

--- www/savePicture.php
+++ www/savePicture.php
@@ -3,7 +3,7 @@ $img = $_POST['Pic'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$success = file_put_contents('face.jpeg', $data);
+$success = file_put_contents('pythonImg/face.jpeg', $data);
 print $success ? $file : 'Unable to save the file.';
 ?>
 
