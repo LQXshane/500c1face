56dc77d84c8240e89d9ad3ea57dd7fb0f21a4e43
Gordon Cai <s20525xx@bu.edu>
2016-04-23 10:35:01 -0400

    Update sendpyimg.php

--- Webdesign/Pres4 website/sendpyimg.php
+++ Webdesign/Pres4 website/sendpyimg.php
@@ -4,7 +4,7 @@ file_put_contents('cc.txt', $_POST['action']);
 
 sleep(1);
 
-$output = shell_exec('./cv.sh');
+$output = shell_exec('python dlib/python_examples/facepy.py face.jpeg cc.txt');
 
 $file = file_get_contents('facepy.jpeg');
 
