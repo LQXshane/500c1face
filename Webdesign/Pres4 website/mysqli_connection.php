a3a1c07ae3ff98cdfbae666c82f82bc979798d99
LQXshane <qxlin@bu.edu>
2016-04-23 18:18:52 -0400

    Security footnote

--- Webdesign/Pres4 website/mysqli_connection.php
+++ Webdesign/Pres4 website/mysqli_connection.php
@@ -9,7 +9,7 @@
         //as is needed.
         //mysql> GRANT INSERT, SELECT, DELETE, UPDATE ON WITCH101.*
     //-> TO 'root'@'localhost'
-    //-> IDENTIFIED BY 'Lp911020';
+    //-> IDENTIFIED BY 'xxxxxxxxxxx';
         //SELECT : Select rows in tables
         //INSERT : Insert new rows into tables
         //UPDATE : Change data in rows
